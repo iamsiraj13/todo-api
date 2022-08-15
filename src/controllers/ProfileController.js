ec5e000e72fc04a5a8dbc2df10026ac55b09b514
iamsiraj13 <iamsiraj13@gmail.co>
2022-08-15 22:56:45 +0600

    User login Functionality with json web token create

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,18 +1,37 @@
 const ProfileModel = require("../models/ProfileModel");
+const jwt = require("jsonwebtoken");
 
-exports.CreateProfile=( req, res )=>{
-    let reqBody = req.body;
-    ProfileModel.create(reqBody,( err, data)=>{
-    if( err ){
-        res.status(400).json({status:'Fail',data:err})
-    }else{
-        res.status(200).json({status:'Success',data})
+exports.CreateProfile = (req, res) => {
+  let reqBody = req.body;
+  ProfileModel.create(reqBody, (err, data) => {
+    if (err) {
+      res.status(400).json({ status: "Fail", data: err });
+    } else {
+      res.status(200).json({ status: "Success", data });
     }
-    })
-}
-exports.UserLogin=( req, res )=>{ 
-    const {UserName,Password}=req.body
+  });
+};
+exports.UserLogin = (req, res) => {
+  const { UserName, Password } = req.body;
+  ProfileModel.find(
+    { UserName: UserName, Password: Password },
+    (error, data) => {
+      if (error) {
+        res.status(400).json({ status: "Login Fail" });
+      } else {
+        if (data.length > 0) {
+          const payload = {
+            exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60),
+            data: data[0],
+          };
 
-    res.json({UserName,Password})
-    
-}
+          const  token = jwt.sign(payload, "jwtsecret");
+
+          res.status(201).json({ status: "Login Success", data:data[0],token });
+        } else {
+          res.status(400).json({ status: "Unauthorized" });
+        }
+      }
+    }
+  );
+};
